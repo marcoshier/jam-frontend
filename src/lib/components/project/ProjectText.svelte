78e0c66127e308ada4f9d976de39957054814463
marco <dellabatemarco@gmail.com>
2025-10-16 18:20:38 +0200

    start project layout

--- src/lib/components/project/ProjectText.svelte
+++ src/lib/components/project/ProjectText.svelte
@@ -3,8 +3,32 @@
     import { selectedId } from "$lib/stores/ui";
     
     const currentProject = $derived($projectsById.get($selectedId));
-    
-    $effect(() => {
-        console.log('Current Project changed:', currentProject);
-    });
 </script>
+
+<div class="project-text-container">
+    <div class="path-container">
+        <h6>PROJECT</h6>
+    </div>
+</div>
+
+
+<style>
+    .project-text-container {
+        height: 100%;
+        width: 100%;
+        padding: 60px;
+        box-sizing: border-box;
+    }
+
+    .path-container {
+        height: 30px;
+    }
+
+    .path-container h6 {
+        color: grey;
+        font-family: monospace;
+        font-size: 11px;
+        font-weight: 400;
+        letter-spacing: 1px;
+    }
+</style>
